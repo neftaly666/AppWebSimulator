a13979078f249206aeed22f08aa5e47f01fe99ac
Dayvid8 <homeboy_u@hotmail.com>
2017-01-15 11:23:28 -0500

    sd
    sd

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :equipment
-  get 'home/index'
-root 'home#index'
+root 'home#sala'
+  
+  get "cuarto" => "home#cuarto", as: :cuarto
+  get "garaje" => "home#garaje", as: :garaje
+  get "cocina" => "home#cocina", as: :cocina
+  get "bano" => "home#bano", as: :bano
+  get "exterior" => "home#exterior", as: :exterior
+  get "recomendaciones" => "home#recomendaciones", as: :recomendaciones
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
