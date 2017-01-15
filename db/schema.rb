e02e459d14dcb1567ddf1424553098317e06289e
Dayvid8 <homeboy_u@hotmail.com>
2017-01-15 11:45:41 -0500

    cv

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115160648) do
-
-  create_table "equipment", force: :cascade do |t|
-    t.string   "equipo"
-    t.integer  "cantidad"
-    t.integer  "tiempo"
-    t.integer  "potencia"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
