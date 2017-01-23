1b74257e9c115bd98b6af61da1fcd96dcd7ae8e1
neftaly666 <dawi6666@outlook.com>
2017-01-23 03:26:24 -0500

    validaciones
    validaciones

--- app/models/component.rb
+++ app/models/component.rb
@@ -1,3 +1,6 @@
 class Component < ApplicationRecord
   validates :equipo, presence: {message: "Campo Obligatorio"}
+  validates_numericality_of :cantidad, :only_integer => true
+  validates_numericality_of :tiempo, :only_integer => true
+  validates_numericality_of :potencia, :only_integer => true
 end
